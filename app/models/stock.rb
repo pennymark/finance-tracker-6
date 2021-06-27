d06f542a0df192d3572e7bf28ea4cfed36845b03
pennymark <33641617+pennymark@users.noreply.github.com>
2021-06-27 14:04:14 -0400

    End of Section 9 - Finance Tracker App

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,27 @@
 class Stock < ApplicationRecord
-    def self.new_lookup(ticker_symbol)
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
+
+
+    def self.new_lookup(ticker_symbol =params[:stock])
+        token = Rails.application.credentials.iex_client[:sandbox_p_api_key]
+
         client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_p_api_key],
                                         secret_token: Rails.application.credentials.iex_client[:sandbox_s_api_key],
-                                        endpoint: 'https://sandbox.iexapis.com')
-        return client.price(ticker_symbol)
+                                        endpoint: 'https://sandbox.iexapis.com/stable/stock/'+ticker_symbol+'/quote?token='+token)
+
+        begin
+            fetch = client.quote(ticker_symbol)
+            new(ticker: ticker_symbol, name: fetch.company_name, last_price: fetch.latest_price)
+        rescue => exception
+            return nil
+        end
+    end
+
+    def self.check_db(ticker_symbol)
+        where(ticker: ticker_symbol).first
     end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stocks', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 end
